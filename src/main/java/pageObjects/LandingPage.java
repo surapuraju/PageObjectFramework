d8ef0797a3526b17c2f1a2fcb1ac5aa2e73dd6ad
Raju Surapuraju <rsurapuraju@gmail.com>
2021-11-13 13:13:14 +0530

    Update LandingPage.java

--- src/main/java/pageObjects/LandingPage.java
+++ src/main/java/pageObjects/LandingPage.java
@@ -3,7 +3,7 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//rahulonlinetutor@gmail.com
+
 public class LandingPage {
 
 	
